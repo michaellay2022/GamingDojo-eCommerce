5069a5cb173525af75bbbe3c75513f40f76cdb50
michaellay2022 <99146295+michaellay2022@users.noreply.github.com>
2022-03-27 18:49:34 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+var banner = document.querySelector('#banner')
+var itemCount = document.querySelector('#itemCount')
+
+// function changes to next pic
+function nextPic() {
+  banner.src = './images/pixel-ninjas-2.png'
+}
+
+function prevPic() {
+  banner.src = './images/stonepunk.png'
+}
+
+function buyItem() {
+  itemCount.innerText++
+}
